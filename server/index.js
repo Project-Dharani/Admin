1f4ee7e96c41c5f58a5eae00cf47eb1d2d5268b8
sham4u <shamiulla388@gmail.com>
2023-06-04 16:15:56 +0530

    started with express server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!");
+});
