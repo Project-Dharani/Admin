682453de4d02e3ead0d59938b3230717b475c2d8
younus123shaik <younus123shaik@gmail.com>
2023-06-04 21:31:58 +0530

    del

--- server/creds.js
+++
@@ -1,5 +0,0 @@
-const serverUrl =
-  "http://connected-python.ckw96vgxltbv.us-east-1.rds.amazonaws.com";
-const userName = "admin";
-const password = "Admin123";
-export default { userName, serverUrl, password };
