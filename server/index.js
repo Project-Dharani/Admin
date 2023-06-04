94e198b848f8a14ca098beb1f312c64192e3adfa
Dharani Gowtham <dharaniadithya1998.da@gmail.com>
2023-06-04 22:14:12 +0530

    accepted incoming changes

--- server/index.js
+++ server/index.js
@@ -2,24 +2,11 @@ import express from "express";
 import creds from "../creds.js";
 import mysql from "mysql";
 const app = express();
-<<<<<<< HEAD
-
-const db = mysql.createConnection({
-  host: creds.serverUrl,
-  user: creds.userName,
-  password: creds.password,
-  database: "test",
-});
-
-app.listen(8800, () => {
-  console.log(creds.userName);
-=======
 const db=mysql.createConnection({
   host: creds.serverUrl,
   user: creds.userName,
   password: creds.password,
   database:"test",
->>>>>>> 624376731b7b661705e11cc4b8ed737dcb6fdb30
 });
 app.listen(8800, () => {
   console.log("backend connected1");
