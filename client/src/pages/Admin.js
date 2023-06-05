68ed56caf95de1f0c6e34f472c1febc7a3c48973
sham4u <shamiulla388@gmail.com>
2023-06-05 11:58:14 +0530

    Replaced Searcg>Add

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -161,7 +161,7 @@ export default function Admin() {
                         }
                       />
                       <Button size="small" onClick={handleAddStudent}>
-                        Search
+                        Add
                       </Button>
                     </Box>
                   )}
